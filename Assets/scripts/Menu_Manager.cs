f7917f1e98177e3e08da3ba6bc2efac879981ff5
Mathias Bergeron <mathias.bergeron05@gmail.com>
2024-12-01 11:12:41 -0500

    Ui
    j'ai add plus de truc au ui

---
+++ Assets/scripts/Menu_Manager.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Menu_Manager : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    private void ChangeScene(int sceneNumb)
+    {
+
+    }
+    void Update()
+    {
+        
+    }
+}
