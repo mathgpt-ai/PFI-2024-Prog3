ef1df31285c984e6a12ee336c09a7be80c0102fe
Mat-Top-G <mathias.bergeron05@gmail.com>
2024-12-04 16:02:43 -0500

    ui
    j'essaye de changer les settings et d'implementer les borings settings tels que controls settings etc

--- Assets/scripts/Game Manager.cs
+++ Assets/scripts/Game Manager.cs
@@ -1,18 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [SerializeField] private Canvas Game;//0
+    [SerializeField] private Canvas Pause;//1
+    [SerializeField] private Canvas Settings;//2
+
+
     void Start()
     {
-        
+        /*ShowCanevas(0)*/;//on met pour que de base ce sois le game UI
     }
+
 
     // Update is called once per frame
     void Update()
     {
-        
+
     }
+   
+
+    private void ShowCanevas(int canevas)
+    {
+        if(canevas == 0)
+        {
+            Game.enabled = true;
+            Pause.enabled = false;
+            Settings.enabled = false;
+        }
+        else if(canevas == 1)
+        {
+            Game.enabled = false;
+            Pause.enabled = true;
+            Settings.enabled = false;
+        }
+        else
+        {
+            Game.enabled=false;
+            Pause.enabled=false;
+            Settings.enabled=true;
+        }
+    }
+
 }
